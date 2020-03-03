dba46bf7243eb33c2a1aeb09bf5a2cee91273754
Ryan Gabbard <gabbard@isi.edu>
2020-03-03 15:15:38 -0800

    Make it easier to access conda script generation

--- pegasus_wrapper/conda_job_script.py
+++ pegasus_wrapper/conda_job_script.py
@@ -7,34 +7,39 @@ from attr import attrs
 from vistautils.parameters import Parameters
 from vistautils.parameters_only_entrypoint import parameters_only_entry_point
 
-import temppathlib
-
 from saga_tools.conda import CondaConfiguration
 from saga_tools.spack import SpackConfiguration
 
 
 def main(params: Parameters):
-    runner = CondaJobScriptGenerator.from_parameters(params)
+    conda_script_generator = CondaJobScriptGenerator.from_parameters(params)
     entry_point = params.string("entry_point")
-    runner.run_entry_point(
+    shell_script = conda_script_generator.generate_shell_script(
         entry_point_name=entry_point,
         param_file=params.existing_file("job_param_file"),
-        conda_script_path=params.creatable_file("conda_script_path"),
         working_directory=params.optional_creatable_directory("working_directory")
         or Path(os.getcwd()),
-        job_name=params.string("job_name", default=entry_point),
-        echo_template=params.boolean("echo_template", default=False),
     )
+
+    params.creatable_file("conda_script_path").write_text(  # type: ignore
+        shell_script, encoding="utf-8"
+    )
+
+    if params.boolean("echo_template", default=False):
+        print(shell_script)
 
 
 @attrs(frozen=True, slots=True)
 class CondaJobScriptGenerator:
     """
-    A driver to create a shell script in order to run a python program in a conda virtual environment.
+    A driver to create a shell script in order to run a python program
+    in a conda virtual environment.
 
-    The program takes a parameters file as input to generate the executable script. The script can be
-    used as an executable job for Pegasus so that a job can be run in a venv as the default pegasus
-    jobs do not run in one.
+    The program takes a parameters file as input to generate the executable script.
+    The script can be
+    used as an executable job for Pegasus
+    so that a job can be run in a venv
+    as the default pegasus jobs do not run in one.
 
     The generated script is based on
     https://github.com/isi-vista/saga-tools/blob/master/saga_tools/slurm_run_python.py
@@ -48,7 +53,8 @@ class CondaJobScriptGenerator:
         environment to run in
     * *spack_environment*: (optional): the spack environment, if any, to run in.
         Cannot appear with *spack_packages*.
-    * *spack_packages*: (optional): a YAML list of Spack packages to load (in *module@version* format).
+    * *spack_packages*: (optional): a YAML list of Spack packages to load
+        (in *module@version* format).
     * *spack_root*: the spack installation to use (necessary only if *spack_environment*
         or *spack_modules*) is specified. This is usually the path to a working
         copy of a spack repository.
@@ -56,11 +62,9 @@ class CondaJobScriptGenerator:
     * *conda_script_path*: the file to write the conda job script to.
     * *job_param_file* (optional): the param file to call
         the job with
-    * *job_name* (optional): The name of the job running via the script, defaults to entry_point
     * *working_directory* (optional)
-    * *echo_template* (optional boolean, default False): whether to echo the generated
-        job script (for debugging).
     """
+
     conda_config: Optional[CondaConfiguration]
     spack_config: Optional[SpackConfiguration]
 
@@ -71,33 +75,17 @@ class CondaJobScriptGenerator:
             spack_config=SpackConfiguration.from_parameters(params),
         )
 
-    def run_entry_point(
-        self,
-        entry_point_name,
-        param_file: Path,
-        conda_script_path: Path,
-        *,
-        working_directory: Path,
-        job_name: str,
-        echo_template: bool = False,
+    def generate_shell_script(
+        self, entry_point_name: str, param_file: Path, *, working_directory: Path
     ):
-        with temppathlib.TmpDirIfNecessary(path=conda_script_path) as tmp_dir:
-            if not conda_script_path:
-                conda_script_path = tmp_dir.path / f"{job_name}.sbatch"
 
-        conda_script_content = CONDA_SCRIPT.format(
+        return CONDA_SCRIPT.format(
             conda_lines=self.conda_config.sbatch_lines(),
             spack_lines=self.conda_config.sbatch_lines(),
             working_directory=working_directory,
             entry_point=entry_point_name,
             param_file=param_file,
         )
-
-        conda_script_path.write_text(  # type: ignore
-            conda_script_content, encoding="utf-8"
-        )
-        if echo_template:
-            print(conda_script_content)
 
 
 CONDA_SCRIPT = """#!/usr/bin/env bash
