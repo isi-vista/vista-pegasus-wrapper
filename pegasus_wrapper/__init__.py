2cb5b8359dc0da9899b139b63be1c9580f8d5727
Ryan Gabbard <gabbard@isi.edu>
2020-03-18 14:39:05 -0700

    Reconcile mine and JAcob's changes

--- pegasus_wrapper/__init__.py
+++ pegasus_wrapper/__init__.py
@@ -17,18 +17,22 @@ Terminology
 # pylint:disable=missing-docstring
 from abc import abstractmethod
 from pathlib import Path
-from typing import Iterable, List, Optional
+from typing import Iterable, Optional
 
-from attr import Factory, attrib, attrs
+from attr import attrib, attrs
 from attr.validators import instance_of
 
 from immutablecollections import ImmutableSet, immutableset
+
+from pegasus_wrapper.pegasus_utils import path_to_pfn
 from vistautils.memory_amount import MemoryAmount
 
-from networkx import DiGraph
-from Pegasus.api import OS, Arch, File, Job
-from Pegasus.DAX3 import ADAG, PFN, Executable, Link
+from Pegasus.api import OS, Arch
+from Pegasus.DAX3 import ADAG, Executable, Link
+from Pegasus.api import File, Job
+from Pegasus.DAX3 import Namespace, Profile
 from pegasus_wrapper.version import version as __version__  # noqa
+from saga_tools.slurm import to_slurm_memory_string
 
 from typing_extensions import Protocol
 
@@ -119,7 +123,7 @@ def script_to_pegasus_executable(
         installed=installed,
         container=container,
     )
-    rtrnr.addPFN(_path_to_pfn(path, site=site))
+    rtrnr.addPFN(path_to_pfn(path, site=site))
     return rtrnr
 
 
@@ -139,108 +143,70 @@ def pegasus_executable_to_pegasus_job(
     return rtrnr
 
 
-def path_to_pegaus_file(
-    path: Path, *, site: str = "local", name: Optional[str] = None
-) -> File:
-    """
-    Given a *path* object return a pegasus `File` for usage in a workflow
 
-    If the resource is not on a local machine provide the *site* string.
-
-    Files can be used for either an input or output of a Job.
-    """
-    rtnr = File(path.stem + path.suffix if name is None else name)
-    rtnr.addPFN(_path_to_pfn(path, site))
-    return rtnr
-
-
-def _path_to_pfn(path: Path, site: str = "local") -> PFN:
-    return PFN(path.absolute(), site=site)
-
-
-@attrs(frozen=True, slots=True)
-class WorkflowBuilder:
-    """
-    A class which wraps a representation of a Pegasus workflow
-
-    Run `execute(workflow)` to write out the workflow into DAX files for submission
-    """
-
-    name: str = attrib(validator=instance_of(str), kw_only=True)
-    created_by: str = attrib(validator=instance_of(str), kw_only=True)
-    log_base_directory: Path = attrib(validator=instance_of(Path), kw_only=True)
-    _graph: DiGraph = attrib(
-        validator=instance_of(DiGraph), kw_only=True, default=DiGraph()
-    )
-    _jobs_in_graph: List[Job] = attrib(
-        validator=instance_of(List), kw_only=True, default=Factory(List), init=False
-    )
-    _files_in_graph: List[File] = attrib(
-        validator=instance_of(List), kw_only=True, default=Factory(List), init=False
-    )
-
-    def schedule_job(self, job: Job, resource_request: ResourceRequest) -> DependencyNode:
-        """
-        Schedule a `Job` for computation during the workflow
-        """
-
-        resource_request.apply_to_job(job, log_base_directory=self.log_base_directory)
-        self._graph.add_node(job)
-        self._jobs_in_graph.append(job)
-        self._add_files_to_graph(job, immutableset(job.get_inputs()), is_input=True)
-        self._add_files_to_graph(job, immutableset(job.get_outputs()), is_input=False)
-
-        return DependencyNode(job=job)
-
-    def _add_files_to_graph(
-        self, job: Job, files: Iterable[File], *, is_input: bool
-    ) -> None:
-        """
-        Add files to the internal digraph to be able to calculate dependent jobs
-        """
-        for file in files:
-            if file not in self._graph:
-                self._graph.add_node(file)
-                self._files_in_graph.append(file)
-
-            if is_input:
-                self._graph.add_edge(file, job, label=INPUT_FILE_LABEL)
-            else:
-                self._graph.add_edge(job, file, label=OUTPUT_FILE_LABEL)
-
-    def get_jobs(self) -> ImmutableSet[Job]:
-        return immutableset(self._jobs_in_graph)
-
-    def get_files(self) -> ImmutableSet[File]:
-        return immutableset(self._files_in_graph)
-
-    def calculate_child_jobs(self, root: Job) -> None:
-        # Is asking the user to know their initial job a problem?
-        raise NotImplementedError()
-
-    def build(self, output_xml_path: Path):
-        """
-        build DAG, call writeXml
-        We will let the user submit themselves, however we could provide them the submit command if known
-        """
-        diamond = ADAG(self.name)
-        diamond.metadata("name", self.name)
-        diamond.metadata("createdby", self.created_by)
-
-        for file in self._files_in_graph:
-            diamond.addFile(file)
-
-        for job in self._jobs_in_graph:
-            diamond.addJob(job)
-
-        # Do Files, Executables, Etc need to be added to a DAX or if we
-        # just add the job do those get automatically added?
-        # If we do need to add these the functions above which create
-        # Executables, files, etc will need to become part of this class so they
-
-        diamond.writeXML(output_xml_path)
-
-        raise NotImplementedError()
+# @attrs(frozen=True, slots=True)
+# class WorkflowBuilder:
+#     """
+#     A class which wraps a representation of a Pegasus workflow
+#
+#     Run `execute(workflow)` to write out the workflow into DAX files for submission
+#     """
+#
+#     name: str = attrib(validator=instance_of(str), kw_only=True)
+#     created_by: str = attrib(validator=instance_of(str), kw_only=True)
+#     log_base_directory: Path = attrib(validator=instance_of(Path), kw_only=True)
+#     _graph: DiGraph = attrib(
+#         validator=instance_of(DiGraph), kw_only=True, default=DiGraph()
+#     )
+#
+#     def schedule_job(self, job: Job, resource_request: ResourceRequest) -> DependencyNode:
+#         """
+#         Schedule a `Job` for computation during the workflow
+#         """
+#
+#         resource_request.apply_to_job(job, log_base_directory=self.log_base_directory)
+#         self._graph.add_node(job)
+#         self._jobs_in_graph.append(job)
+#         self._add_files_to_graph(job, immutableset(job.get_inputs()), is_input=True)
+#         self._add_files_to_graph(job, immutableset(job.get_outputs()), is_input=False)
+#
+#         return DependencyNode(job=job)
+#
+#     def _add_files_to_graph(
+#         self, job: Job, files: Iterable[File], *, is_input: bool
+#     ) -> None:
+#         """
+#         Add files to the internal digraph to be able to calculate dependent jobs
+#         """
+#         resource_request.apply_to_job(
+#             job,
+#             self.log_base_directory,
+#             job_name=job_name if job_name is not None else None,
+#         )
+#
+#     def build(self, output_xml_path: Path):
+#         """
+#         build DAG, call writeXml
+#         We will let the user submit themselves, however we could provide them the submit command if known
+#         """
+#         diamond = ADAG(self.name)
+#         diamond.metadata("name", self.name)
+#         diamond.metadata("createdby", self.created_by)
+#
+#         for file in self._files_in_graph:
+#             diamond.addFile(file)
+#
+#         for job in self._jobs_in_graph:
+#             diamond.addJob(job)
+#
+#         # Do Files, Executables, Etc need to be added to a DAX or if we
+#         # just add the job do those get automatically added?
+#         # If we do need to add these the functions above which create
+#         # Executables, files, etc will need to become part of this class so they
+#
+#         diamond.writeXML(output_xml_path)
+#
+#         raise NotImplementedError()
 
 
 INPUT_FILE_LABEL = "input_file"
