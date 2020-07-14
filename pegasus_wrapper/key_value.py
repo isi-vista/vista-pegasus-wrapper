4eb6ad1a0244b78d1860372411ef3f60475dce92
Joe Cummings <jrcummings27@gmail.com>
2020-07-14 16:55:48 -0500

    Cannot modify immutable parameters

--- pegasus_wrapper/key_value.py
+++ pegasus_wrapper/key_value.py
@@ -110,17 +110,18 @@ def split_key_value_store(
 
     split_locator = input_store.locator / "split"
     split_output_dir = directory_for(split_locator)
-    params = Parameters.from_mapping(
-        {
-            "input": input_store.input_parameters(),
-            "num_slices": num_parts,
-            "output_dir": split_output_dir,
-        }
-    )
+    param_args = {
+        "input": input_store.input_parameters(),
+        "num_slices": num_parts,
+        "output_dir": split_output_dir,
+    }
     if random_seed:
-        params["random_seed"] = random_seed
+        param_args["random_seed"] = random_seed
     split_job = run_python_on_parameters(
-        split_locator, split_entry_point, params, depends_on=input_store
+        split_locator,
+        split_entry_point,
+        Parameters.from_mapping(param_args),
+        depends_on=input_store,
     )
     return tuple(
         ZipKeyValueStore(
