ac117047f2de7af5350fbdd38c67d0a401f4ed31
Joseph Cecil <jcecil@isi.edu>
2020-08-04 13:39:14 -0400

    Make black/isort happy

--- pegasus_wrapper/resource_request.py
+++ pegasus_wrapper/resource_request.py
@@ -152,9 +152,7 @@ class SlurmResourceRequest(ResourceRequest):
         job.addProfile(
             Profile(Namespace.PEGASUS, "glite.arguments", slurm_resource_content)
         )
-        job.addProfile(
-            Profile(Namespace.DAGMAN, "category", self.partition)
-        )
+        job.addProfile(Profile(Namespace.DAGMAN, "category", self.partition))
 
 
 SLURM_RESOURCE_STRING = """--{qos_or_account} --partition {partition} --ntasks 1
--- pegasus_wrapper/workflow.py
+++ pegasus_wrapper/workflow.py
@@ -15,6 +15,7 @@ from vistautils.class_utils import fully_qualified_name
 from vistautils.io_utils import CharSink
 from vistautils.parameters import Parameters, YAMLParametersWriter
 
+from Pegasus.DAX3 import ADAG, Executable, File, Job, Link, Namespace, Profile
 from pegasus_wrapper import resources
 from pegasus_wrapper.artifact import DependencyNode, _canonicalize_depends_on
 from pegasus_wrapper.conda_job_script import CondaJobScriptGenerator
@@ -25,8 +26,6 @@ from pegasus_wrapper.pegasus_utils import (
     path_to_pfn,
 )
 from pegasus_wrapper.resource_request import ResourceRequest
-
-from Pegasus.DAX3 import ADAG, Executable, File, Job, Link, Namespace, Profile
 from saga_tools.conda import CondaConfiguration
 
 try:
