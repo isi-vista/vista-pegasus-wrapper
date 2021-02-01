ff1712947e4e2ef5cee36aa2687b6aa7ca11ad3d
Jacob Lichtefeld <jalichtefeld@gmail.com>
2021-02-01 07:23:42 -0800

    Update requirements

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     name="pegasus_wrapper",
     version=version,  # noqa
     author="Jacob Lichtefeld, Ryan Gabbard",
-    author_email="jalichtefeld@gmail.com,gabbard@isi.edu",
+    author_email="jalichtefeld@gmail.com,ryan.gabbard@gmail.com",
     description="A higher-level API for ISI Pegasus, adapted to the quirks of the ISI Vista group",
     url="https://github.com/isi-vista/vista-pegasus-wrapper",
     packages=find_packages(),
@@ -25,6 +25,7 @@ setup(
         "importlib-resources==1.4.0",
 	    "vistautils>=0.21.0",
         "gitpython==3.1.12",
+        "pegasus-wms.api==5.0.0",
     ],
     classifiers=[
         "Programming Language :: Python :: 3",
